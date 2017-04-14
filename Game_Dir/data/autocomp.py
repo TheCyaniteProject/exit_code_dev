bc7d7b54a07b094064dd275d4e28ab13490abc9f
Simon Schuerrle <sitischu@hotmail.de>
2017-04-15 01:47:14 +0200

    Changed indentation to spaces and fixed files
    Changed the Indentation to spaces in all Files
    Fixed the new_player_ui.py so it works in py3
    Fixed the sys_make.py so it work in py3

--- Game_Dir/data/autocomp.py
+++ Game_Dir/data/autocomp.py
@@ -25,61 +25,61 @@ __version__ = "1.0"
 tkinter_umlauts=['odiaeresis', 'adiaeresis', 'udiaeresis', 'Odiaeresis', 'Adiaeresis', 'Udiaeresis', 'ssharp']
 
 class AutocompleteEntry(Tkinter.Entry):
-	"""
-	Subclass of Tkinter.Entry that features autocompletion.
-	
-	To enable autocompletion use set_completion_list(list) to define 
-	a list of possible strings to hit.
-	To cycle through hits use down and up arrow keys.
-	"""
-	def set_completion_list(self, completion_list):
-			self._completion_list = completion_list
-			self._hits = []
-			self._hit_index = 0
-			self.position = 0
-			self.bind('<KeyRelease>', self.handle_keyrelease)               
+    """
+    Subclass of Tkinter.Entry that features autocompletion.
 
-	def autocomplete(self, delta=0):
-			"""autocomplete the Entry, delta may be 0/1/-1 to cycle through possible hits"""
-			if delta: # need to delete selection otherwise we would fix the current position
-					self.delete(self.position, Tkinter.END)
-			else: # set position to end so selection starts where textentry ended
-					self.position = len(self.get())
-			# collect hits
-			_hits = []
-			for element in self._completion_list:
-					if element.startswith(self.get().lower()):
-							_hits.append(element)
-			# if we have a new hit list, keep this in mind
-			if _hits != self._hits:
-					self._hit_index = 0
-					self._hits=_hits
-			# only allow cycling if we are in a known hit list
-			if _hits == self._hits and self._hits:
-					self._hit_index = (self._hit_index + delta) % len(self._hits)
-			# now finally perform the auto completion
-			if self._hits:
-					self.delete(0,Tkinter.END)
-					self.insert(0,self._hits[self._hit_index])
-					self.select_range(self.position,Tkinter.END)
-					
-	def handle_keyrelease(self, event):
-			"""event handler for the keyrelease event on this widget"""
-			"""if event.keysym == "BackSpace":
-					self.delete(self.index(Tkinter.INSERT), Tkinter.END) 
-					self.position = self.index(Tkinter.END)"""
-			"""if event.keysym == "Left":
-					if self.position < self.index(Tkinter.END): # delete the selection
-							self.delete(self.position, Tkinter.END)
-					else:
-							self.position = self.position-1 # delete one character
-							self.delete(self.position, Tkinter.END)"""
-			"""if event.keysym == "Right":
-					self.position = self.index(Tkinter.END) # go to end (no selection)"""
-			if event.keysym == "Down":
-					self.autocomplete(1) # cycle to next hit
-			if event.keysym == "Up":
-					self.autocomplete(-1) # cycle to previous hit
-			# perform normal autocomplete if event is a single key or an umlaut
-			if len(event.keysym) == 1 or event.keysym in tkinter_umlauts:
-					self.autocomplete()
+    To enable autocompletion use set_completion_list(list) to define
+    a list of possible strings to hit.
+    To cycle through hits use down and up arrow keys.
+    """
+    def set_completion_list(self, completion_list):
+            self._completion_list = completion_list
+            self._hits = []
+            self._hit_index = 0
+            self.position = 0
+            self.bind('<KeyRelease>', self.handle_keyrelease)
+
+    def autocomplete(self, delta=0):
+            """autocomplete the Entry, delta may be 0/1/-1 to cycle through possible hits"""
+            if delta: # need to delete selection otherwise we would fix the current position
+                    self.delete(self.position, Tkinter.END)
+            else: # set position to end so selection starts where textentry ended
+                    self.position = len(self.get())
+            # collect hits
+            _hits = []
+            for element in self._completion_list:
+                    if element.startswith(self.get().lower()):
+                            _hits.append(element)
+            # if we have a new hit list, keep this in mind
+            if _hits != self._hits:
+                    self._hit_index = 0
+                    self._hits=_hits
+            # only allow cycling if we are in a known hit list
+            if _hits == self._hits and self._hits:
+                    self._hit_index = (self._hit_index + delta) % len(self._hits)
+            # now finally perform the auto completion
+            if self._hits:
+                    self.delete(0,Tkinter.END)
+                    self.insert(0,self._hits[self._hit_index])
+                    self.select_range(self.position,Tkinter.END)
+
+    def handle_keyrelease(self, event):
+            """event handler for the keyrelease event on this widget"""
+            """if event.keysym == "BackSpace":
+                    self.delete(self.index(Tkinter.INSERT), Tkinter.END)
+                    self.position = self.index(Tkinter.END)"""
+            """if event.keysym == "Left":
+                    if self.position < self.index(Tkinter.END): # delete the selection
+                            self.delete(self.position, Tkinter.END)
+                    else:
+                            self.position = self.position-1 # delete one character
+                            self.delete(self.position, Tkinter.END)"""
+            """if event.keysym == "Right":
+                    self.position = self.index(Tkinter.END) # go to end (no selection)"""
+            if event.keysym == "Down":
+                    self.autocomplete(1) # cycle to next hit
+            if event.keysym == "Up":
+                    self.autocomplete(-1) # cycle to previous hit
+            # perform normal autocomplete if event is a single key or an umlaut
+            if len(event.keysym) == 1 or event.keysym in tkinter_umlauts:
+                    self.autocomplete()
--- Game_Dir/data/settings.py
+++ Game_Dir/data/settings.py
@@ -19,7 +19,7 @@ import random
 """========================================================="""
 
 def get_time():
-	return strftime('%Y-%m-%d %H:%M.%S')
+    return strftime('%Y-%m-%d %H:%M.%S')
 
 print 'Loading Game @ %s\n%s\n\nHello world!\n' %  (get_time(),os.getcwd())
 
@@ -44,7 +44,7 @@ exp_multi   = 100
 player_ip   = '0.0.0.0'
 current_ip = player_ip
 
-email_ac =	  None
+email_ac =    None
 payme_ac =    None
 easybits_ac = None
 
@@ -63,30 +63,30 @@ bits_min =    0.000001
 
 """==========
 Payme \ Bits Converion Rates
-Payme \ Bits: 100.0 \ 1.0 (payme / 100.0)	
-Bits \ Payme: 1.0 \ 100.0 (bits * 100.0)	
+Payme \ Bits: 100.0 \ 1.0 (payme / 100.0)
+Bits \ Payme: 1.0 \ 100.0 (bits * 100.0)
 =========="""
 
 # Other Variable Creation
 systemRestart = False
 user_home_dir = '\\systems\\user_home\\os_root_dir'
-system_id  =	''
-file_types =	('.sh', '.txt', '.log', '.app', '.contact')
-text_types =	('.sh', '.txt', '.log')
+system_id  =    ''
+file_types =    ('.sh', '.txt', '.log', '.app', '.contact')
+text_types =    ('.sh', '.txt', '.log')
 
-pronouns   =	{
-					'female': ['She', 'Her', 'Her\'s'],
-					'male': ['He', 'His', 'His'],
-					'floofball': ['"RainbowDashies\' Pyromancing Floof-ball"',
-					'"RainbowDashies\' Pyromancing Floof-ball"\'s',
-					'"RainbowDashies\' Pyromancing Floof-ball"\'s']
-				}
+pronouns   =    {
+                    'female': ['She', 'Her', 'Her\'s'],
+                    'male': ['He', 'His', 'His'],
+                    'floofball': ['"RainbowDashies\' Pyromancing Floof-ball"',
+                    '"RainbowDashies\' Pyromancing Floof-ball"\'s',
+                    '"RainbowDashies\' Pyromancing Floof-ball"\'s']
+                }
 
 # System stuff
-world_IPs =		[]
-discovered_IPs=	[]
-visited_IPs =	[]
-max_ip_gen = 	15
+world_IPs =     []
+discovered_IPs= []
+visited_IPs =   []
+max_ip_gen =    15
 
 
 # Other
@@ -97,141 +97,141 @@ seeded.seed(game_seed)
 print 'Done Loading Defaults.'
 
 def hashpass(key):
-	key = key.strip().rstrip()
-	sha = hashlib.sha256(key.encode()).hexdigest()
-	return sha
+    key = key.strip().rstrip()
+    sha = hashlib.sha256(key.encode()).hexdigest()
+    return sha
 
 """ Loading Settings """
 def load_game_settings():
-	print 'Loading Game Config..'
-	global user_dir
-	global game_seed
-	global default_user
-	global username
-	global user_home_dir
-	if not os.path.isfile(settings_file) == True:
-		print('error: settings file not found! - "%s\\%s" - falling back to defaults' % (game_dir,settings_file))
-	else:
-		conf.read(settings_file)
-		if 'system' in conf.sections():
-			items = [i for (i,n) in conf['system'].items()]
-			if 'default_user' in items:
-				default_user = conf['system']['default_user']
-				username = default_user
-			if 'user_dir' in items:
-				user_dir = conf['system']['user_dir']
-			if 'user_home_dir' in items:
-				user_home_dir = conf['system']['user_home_dir']
-			if 'default_seed' in items:
-				game_seed = conf['system']['default_seed']
+    print 'Loading Game Config..'
+    global user_dir
+    global game_seed
+    global default_user
+    global username
+    global user_home_dir
+    if not os.path.isfile(settings_file) == True:
+        print('error: settings file not found! - "%s\\%s" - falling back to defaults' % (game_dir,settings_file))
+    else:
+        conf.read(settings_file)
+        if 'system' in conf.sections():
+            items = [i for (i,n) in conf['system'].items()]
+            if 'default_user' in items:
+                default_user = conf['system']['default_user']
+                username = default_user
+            if 'user_dir' in items:
+                user_dir = conf['system']['user_dir']
+            if 'user_home_dir' in items:
+                user_home_dir = conf['system']['user_home_dir']
+            if 'default_seed' in items:
+                game_seed = conf['system']['default_seed']
 
-	if '%game_dir%' in user_dir:
-		user_dir = ''.join((game_dir, user_dir.split('%game_dir%')[1]))
-	print 'Done Loading Game Config.'
-	
+    if '%game_dir%' in user_dir:
+        user_dir = ''.join((game_dir, user_dir.split('%game_dir%')[1]))
+    print 'Done Loading Game Config.'
+
 def load_player_settings():
-	print 'Loading Player Config..'
-	global username
-	global game_seed
-	global player_rank
-	global player_exp
-	global bits
-	global payme
-	global exp_multi
-	global payme_ac
-	global easybits_ac
-	global email_ac
-	global player_ip
-	if not os.path.isfile(('%s%s\\user.ini' % (user_dir,username.lower())).split(game_dir+'\\')[1]) == True:
-		print('error: settings file not found! - "%s" - falling back to defaults' % (('%s%s\\user.ini' % (user_dir,username.lower())).split(game_dir+'\\')[1]))
-	else:
-		conf.read(('%s%s\\user.ini' % (user_dir,username.lower())).split(game_dir+'\\')[1])
-		if 'info' in conf.sections():
-			items = [i for (i,n) in conf['info'].items()]
-			if 'username' in items:
-				username = conf['info']['username']
-			if 'seed' in items:
-				game_seed = conf['info']['seed']
-			if 'ranking' in items:
-				player_rank = int(conf['info']['ranking'])
-			if 'exp' in items:
-				player_exp = int(conf['info']['exp'])
-			if 'easybits_ac' in items:
-				easybits_ac = conf['info']['easybits_ac']
-			if 'payme_ac' in items:
-				payme_ac = conf['info']['payme_ac']
-			if 'email_ac' in items:
-				email_ac = conf['info']['email_ac']
-			if 'player_ip' in items:
-				player_ip = conf['info']['player_ip']
-	
-	exp_multi = exp_multi*player_rank
-	print 'Done Loading Player Config.'
+    print 'Loading Player Config..'
+    global username
+    global game_seed
+    global player_rank
+    global player_exp
+    global bits
+    global payme
+    global exp_multi
+    global payme_ac
+    global easybits_ac
+    global email_ac
+    global player_ip
+    if not os.path.isfile(('%s%s\\user.ini' % (user_dir,username.lower())).split(game_dir+'\\')[1]) == True:
+        print('error: settings file not found! - "%s" - falling back to defaults' % (('%s%s\\user.ini' % (user_dir,username.lower())).split(game_dir+'\\')[1]))
+    else:
+        conf.read(('%s%s\\user.ini' % (user_dir,username.lower())).split(game_dir+'\\')[1])
+        if 'info' in conf.sections():
+            items = [i for (i,n) in conf['info'].items()]
+            if 'username' in items:
+                username = conf['info']['username']
+            if 'seed' in items:
+                game_seed = conf['info']['seed']
+            if 'ranking' in items:
+                player_rank = int(conf['info']['ranking'])
+            if 'exp' in items:
+                player_exp = int(conf['info']['exp'])
+            if 'easybits_ac' in items:
+                easybits_ac = conf['info']['easybits_ac']
+            if 'payme_ac' in items:
+                payme_ac = conf['info']['payme_ac']
+            if 'email_ac' in items:
+                email_ac = conf['info']['email_ac']
+            if 'player_ip' in items:
+                player_ip = conf['info']['player_ip']
+
+    exp_multi = exp_multi*player_rank
+    print 'Done Loading Player Config.'
 load_game_settings()
 load_player_settings()
-#os.system('pause')		# Debug
+#os.system('pause')     # Debug
 
 """
 login_creds = {
-	'cyanite':{
-		'email.com':[True, 'cyanite@email.com'],
-		'easybits.com':[True, 'cyanite_GHpNWEx8BQ0'],
-		'payme.net':[True, 'cyanite_GHpNWEx8BQ0']
-		}
-	}"""
+    'cyanite':{
+        'email.com':[True, 'cyanite@email.com'],
+        'easybits.com':[True, 'cyanite_GHpNWEx8BQ0'],
+        'payme.net':[True, 'cyanite_GHpNWEx8BQ0']
+        }
+    }"""
 
 login_creds = {username.lower():{}}
 
 # User Wallet Loading
 if not payme_ac == None:
-	with open('%s%s\\wallets\\payme\\%s.ini' % (user_dir,username.lower(),payme_ac), 'r') as f:
-		try:
-			payme = float(f.readlines()[0].strip().rstrip())
-			login_creds[username.lower()]['payme.net'] = [True, str(payme_ac.strip().rstrip())]
-		except: print '!!!Could not load Payme Balence!!!'
+    with open('%s%s\\wallets\\payme\\%s.ini' % (user_dir,username.lower(),payme_ac), 'r') as f:
+        try:
+            payme = float(f.readlines()[0].strip().rstrip())
+            login_creds[username.lower()]['payme.net'] = [True, str(payme_ac.strip().rstrip())]
+        except: print '!!!Could not load Payme Balence!!!'
 
 if not easybits_ac == None:
-	with open('%s%s\\wallets\\easybits\\%s.ini' % (user_dir,username.lower(),easybits_ac), 'r') as f:
-		try:
-			bits = float(f.readlines()[0].strip().rstrip())
-			login_creds[username.lower()]['easybits.com'] = [True, str(easybits_ac.strip().rstrip())]
-		except: print '!!!Could not load Easybits Balence!!!'
+    with open('%s%s\\wallets\\easybits\\%s.ini' % (user_dir,username.lower(),easybits_ac), 'r') as f:
+        try:
+            bits = float(f.readlines()[0].strip().rstrip())
+            login_creds[username.lower()]['easybits.com'] = [True, str(easybits_ac.strip().rstrip())]
+        except: print '!!!Could not load Easybits Balence!!!'
 
 # User Email Loading
 if not email_ac == None:
-	try:login_creds[username.lower()]['email.com'] = [True, str(email_ac.strip().rstrip())]
-	except: print '!!!Could not load Email account!!!'
+    try:login_creds[username.lower()]['email.com'] = [True, str(email_ac.strip().rstrip())]
+    except: print '!!!Could not load Email account!!!'
 
 # Load Existing IPs
 def load_world_IPs():
-	global world_IPs
-	world_IPs = []
-	if os.path.isfile('%s%s\\systems\\worldips.ini' % (user_dir,username.lower())):
-		with open('%s%s\\systems\\worldips.ini' % (user_dir,username.lower()), 'r') as f:
-			for system in f.readlines():
-				world_IPs.append(system.strip().rstrip())
+    global world_IPs
+    world_IPs = []
+    if os.path.isfile('%s%s\\systems\\worldips.ini' % (user_dir,username.lower())):
+        with open('%s%s\\systems\\worldips.ini' % (user_dir,username.lower()), 'r') as f:
+            for system in f.readlines():
+                world_IPs.append(system.strip().rstrip())
 load_world_IPs()
 
 def load_discovered_IPs():
-	global discovered_IPs
-	discovered_IPs = []
-	if os.path.isfile('%s%s\\systems\\discoveredips.ini' % (user_dir,username.lower())):
-		with open('%s%s\\systems\\discoveredips.ini' % (user_dir,username.lower()), 'r') as f:
-			for system in f.readlines():
-				if system.strip().rstrip() in world_IPs:
-					discovered_IPs.append(system.strip().rstrip())
-				else: print 'orphaned system: %s from DISCOVERED' % system
+    global discovered_IPs
+    discovered_IPs = []
+    if os.path.isfile('%s%s\\systems\\discoveredips.ini' % (user_dir,username.lower())):
+        with open('%s%s\\systems\\discoveredips.ini' % (user_dir,username.lower()), 'r') as f:
+            for system in f.readlines():
+                if system.strip().rstrip() in world_IPs:
+                    discovered_IPs.append(system.strip().rstrip())
+                else: print 'orphaned system: %s from DISCOVERED' % system
 load_discovered_IPs()
 
 def load_visited_IPs():
-	global visited_IPs
-	visited_IPs = []
-	if os.path.isfile('%s%s\\systems\\visitedips.ini' % (user_dir,username.lower())):
-		with open('%s%s\\systems\\visitedips.ini' % (user_dir,username.lower()), 'r') as f:
-			for system in f.readlines():
-				if system.strip().rstrip() in world_IPs:
-					visited_IPs.append(system.strip().rstrip())
-				else: print 'orphaned system: %s from VISITED' % system
+    global visited_IPs
+    visited_IPs = []
+    if os.path.isfile('%s%s\\systems\\visitedips.ini' % (user_dir,username.lower())):
+        with open('%s%s\\systems\\visitedips.ini' % (user_dir,username.lower()), 'r') as f:
+            for system in f.readlines():
+                if system.strip().rstrip() in world_IPs:
+                    visited_IPs.append(system.strip().rstrip())
+                else: print 'orphaned system: %s from VISITED' % system
 load_visited_IPs()
 
 # ===== Debug Output
@@ -249,7 +249,7 @@ print '=====\nAccount Balance: %s | Payme: %s - Bits: %s\n=====\n' % (username,p
 
 working_dir = os.path.join(user_dir,username.lower(),'systems\\user_home\\os_root_dir')
 system_home = working_dir # Current Dir
-system_ip = player_ip     # Current System IP 
+system_ip = player_ip     # Current System IP
 current_ip = system_ip    # Spoofable IP
 player_home = working_dir # Never changes
 print 'Moved player @ %s' % working_dir
