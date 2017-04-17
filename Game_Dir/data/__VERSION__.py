4495265236fb3bd55d2ac801ac05b004509ea2ff
Simon Schürrle <sitischu@hotmail.de>
2017-04-17 22:32:03 +0200

    Removed the License mention from the Version File

--- Game_Dir/data/__VERSION__.py
+++ Game_Dir/data/__VERSION__.py
@@ -1,7 +1,1 @@
-"""============================================================
-This file is licensed under the "GNU General Public License v3.0"
-And is provided by;
-"Allison Marie Bennett", TheCyaniteProject@gmail.com
-============================================================"""
-
-__VERSION__ = 'PRE-ALPHA-1.5' # First number = MAJOR change - Second number = Feature, bugfix, code-edit
+__VERSION__ = 'PRE-ALPHA-1.5'  # First number = MAJOR change - Second number = Feature, bugfix, code-edit
